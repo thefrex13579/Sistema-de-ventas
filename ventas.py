2995c92d0eae0ecf3873d9c04b19c493ad771292
kirbynutella <franchescaalejandracocom@gmail.com>
2025-07-02 00:19:25 -0400

    Add files via upload

--- ventas.py
+++ ventas.py
@@ -25,8 +25,59 @@ class Ventas(tk.Frame):
 
         label_numero_factura = tk.Label(lblframe, text="Numero de \nfactura", bg="#C6D9E3", font="sans 12 bold")
         label_numero_factura.place(x=10, y=5)
-
         self.numero_factura = tk.StringVar()
 
         self.entry_numero_factura = ttk.Entry(lblframe, textvariable=self.numero_factura, state="readonly", font="sans 12 bold")
         self.entry_numero_factura.place(x=100, y=5, width=80)
+
+        label_nombre = tk.Label(lblframe, text="Productos: ", bg="#C6D9E3", font="sans 12 bold")
+        label_nombre.place(x=200, y=12)
+        self.entry_nombre = ttk.Entry(lblframe, font="sans 12 bold")
+        self.entry_nombre.place(x=280, y=10, width=180)
+
+        label_valor = tk.Label(lblframe, text="Precio", bg="#C6D9E3", font="sans 12 bold")
+        label_valor.place(x=470, y=12)
+        self.entry_valor= ttk.Entry(lblframe, font="sans 12 bold")
+        self.entry_valor.place(x=540, y=10, width=180)
+
+        label_cantidad = tk.Label(lblframe, text="Cantidad: ", bg= "#C6D9E3", font="sans 12 bold")
+        label_cantidad.place(x=730, y=12)
+        self.entry_cantidad = ttk.Entry(lblframe, font="sans 12 bold")
+        self.entry_cantidad.place(x=820, y=10)
+
+        treFrame= tk.Frame(frame2, bg= "#C6D9E3")
+        treFrame.place(x=150, y=120)
+
+        scrol_y= ttk.Scrollbar(treFrame, orient=VERTICAL)
+        scrol_y.pack(side=RIGHT, fill=Y)
+
+        scrol_x= ttk.Scrollbar(treFrame, orient=HORIZONTAL)
+        scrol_x.pack(side=BOTTOM, fill=X)
+
+        self.tree = ttk.Treeview(treFrame, columns=("Producto", "Precio", "Cantidad", "Subtotal"), show="headings", height=10, yscrollcommand=scrol_y.set, xscrollcommand=scrol_x.set)
+        scrol_y.config(command=self.tree.yview)
+        scrol_x.config(command=self.tree.xview)
+
+        self.tree.heading("#1", text="Producto")
+        self.tree.heading("#2", text="Precio")
+        self.tree.heading("#3", text="Cantidad")
+        self.tree.heading("#4", text="Subtotal")
+
+        self.tree.column("Producto", anchor="center")
+        self.tree.column("Precio", anchor="center")
+        self.tree.column("Cantidad", anchor="center")
+        self.tree.column("Subtotal", anchor="center")
+
+        self.tree.pack(expand=True, fill=BOTH)
+
+        lblframe1 = LabelFrame(frame2, text="Opciones", bg= "#dddddd", font="sans 12 bold")
+        lblframe1.place(x=10, y=380, width=1060, height=100)
+
+        boton_agregar = tk.Button(lblframe1, text="Agregar articulo", bg= "#dddddd", font="sans 12 bold")
+        boton_agregar.place(x=50, y=10, width=240, height=50)
+
+        boton_pagar = tk.Button(lblframe1, text="Pagar", bg= "#dddddd", font="sans 12 bold")
+        boton_pagar.place(x=400, y=10, width=240, height=50)
+
+        boton_ver_facturas = tk.Button(lblframe1, text="Ver facturas", bg= "#dddddd", font="sans 12 bold")
+        boton_ver_facturas.place(x=750, y=10, width=240, height=50)
